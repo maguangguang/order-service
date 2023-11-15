ffd11eb65d25e6eee28ad4bdb1b46b7d1b89cddc
Guangguang Ma <ggma@thoughtworks.com>
2023-11-15 16:38:47 +0800

    feat: add API for cancel order

--- src/main/java/com/tw/flyhigh/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tw/flyhigh/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.tw.flyhigh.common.enums.OrderStatusEnum;
 import com.tw.flyhigh.common.exception.BusinessException;
 import com.tw.flyhigh.common.exception.ErrorCode;
 import com.tw.flyhigh.common.exception.NoMoreSeatException;
+import com.tw.flyhigh.common.exception.OrderNotFoundException;
 import com.tw.flyhigh.dto.CreateOrderDto;
 import com.tw.flyhigh.dto.FlightDetail;
 import com.tw.flyhigh.dto.Order;
@@ -29,80 +30,111 @@ import java.util.stream.Collectors;
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    private final TicketOrderRepository orderRepository;
-    private final TicketOrderEventRepository ticketOrderEventRepository;
-    private final PriceSeatManagerClient priceSeatManagerClient;
-    private final AmqpMessageServiceImpl amqpMessageServiceImpl;
+  private final TicketOrderRepository orderRepository;
+  private final TicketOrderEventRepository ticketOrderEventRepository;
+  private final PriceSeatManagerClient priceSeatManagerClient;
+  private final AmqpMessageServiceImpl amqpMessageServiceImpl;
 
-    public OrderServiceImpl(TicketOrderRepository orderRepository, TicketOrderEventRepository ticketOrderEventRepository, PriceSeatManagerClient priceSeatManagerClient, AmqpMessageServiceImpl amqpMessageServiceImpl) {
-        this.orderRepository = orderRepository;
-        this.ticketOrderEventRepository = ticketOrderEventRepository;
-        this.priceSeatManagerClient = priceSeatManagerClient;
-        this.amqpMessageServiceImpl = amqpMessageServiceImpl;
+  public OrderServiceImpl(
+      TicketOrderRepository orderRepository,
+      TicketOrderEventRepository ticketOrderEventRepository,
+      PriceSeatManagerClient priceSeatManagerClient,
+      AmqpMessageServiceImpl amqpMessageServiceImpl) {
+    this.orderRepository = orderRepository;
+    this.ticketOrderEventRepository = ticketOrderEventRepository;
+    this.priceSeatManagerClient = priceSeatManagerClient;
+    this.amqpMessageServiceImpl = amqpMessageServiceImpl;
+  }
+
+  @Override
+  public Order createOrder(CreateOrderDto createOrderDto) {
+    ReserveSeatRequest reserveSeatRequest =
+        new ReserveSeatRequest(
+            createOrderDto.getFlight(),
+            createOrderDto.getClassType(),
+            createOrderDto.getPassengerDtoList().size());
+    boolean reserveSuccess = priceSeatManagerClient.reserveSeat(reserveSeatRequest);
+    if (!reserveSuccess) {
+      throw new NoMoreSeatException();
     }
+    TicketOrderEntity ticketOrderEntity = transferToOrderEntity(createOrderDto);
+    List<PassengerEntity> passengerEntities =
+        createOrderDto.getPassengerDtoList().stream()
+            .map(
+                passenger -> {
+                  PassengerEntity passengerEntity = new PassengerEntity();
+                  BeanUtils.copyProperties(passenger, passengerEntity);
+                  passengerEntity.setOrder(ticketOrderEntity);
+                  return passengerEntity;
+                })
+            .collect(Collectors.toList());
+    TicketOrderEventEntity ticketOrderEventEntity =
+        TicketOrderEventEntity.builder().status(OrderStatusEnum.CREATED.name()).build();
+    ticketOrderEventEntity.setOrder(ticketOrderEntity);
+    ticketOrderEntity.setPassengers(passengerEntities);
+    ticketOrderEntity.setEvents(Collections.singletonList(ticketOrderEventEntity));
+    orderRepository.save(ticketOrderEntity);
 
-    @Override
-    public Order createOrder(CreateOrderDto createOrderDto) {
-        ReserveSeatRequest reserveSeatRequest = new ReserveSeatRequest(createOrderDto.getFlight(), createOrderDto.getClassType(),
-                                                                       createOrderDto.getPassengerDtoList().size());
-        boolean reserveSuccess = priceSeatManagerClient.reserveSeat(reserveSeatRequest);
-        if (!reserveSuccess) {
-            throw new NoMoreSeatException();
-        }
-        TicketOrderEntity ticketOrderEntity = transferToOrderEntity(createOrderDto);
-        List<PassengerEntity> passengerEntities = createOrderDto.getPassengerDtoList().stream().map(passenger -> {
-            PassengerEntity passengerEntity = new PassengerEntity();
-            BeanUtils.copyProperties(passenger, passengerEntity);
-            passengerEntity.setOrder(ticketOrderEntity);
-            return passengerEntity;
-        }).collect(Collectors.toList());
-        TicketOrderEventEntity ticketOrderEventEntity = TicketOrderEventEntity.builder().status(OrderStatusEnum.CREATED.name()).build();
-        ticketOrderEventEntity.setOrder(ticketOrderEntity);
-        ticketOrderEntity.setPassengers(passengerEntities);
-        ticketOrderEntity.setEvents(Collections.singletonList(ticketOrderEventEntity));
-        orderRepository.save(ticketOrderEntity);
+    //        publish(ticketOrderEntity, OrderStatusEnum.CREATED.name());
+    return transferOrder(ticketOrderEntity);
+  }
 
-//        publish(ticketOrderEntity, OrderStatusEnum.CREATED.name());
-        return transferOrder(ticketOrderEntity);
+  private TicketOrderEntity transferToOrderEntity(CreateOrderDto createOrderDto) {
+    return TicketOrderEntity.builder()
+        .userId(createOrderDto.getUserId())
+        .flight(createOrderDto.getFlight())
+        .classType(createOrderDto.getClassType())
+        .contactName(createOrderDto.getContactName())
+        .contactMobile(createOrderDto.getContactMobile())
+        .status(OrderStatusEnum.CREATED.name())
+        .build();
+  }
+
+  private void publish(TicketOrderEntity ticketOrderEntity, String status) {
+    OrderMessage orderMessage = OrderMessage.from(ticketOrderEntity);
+    orderMessage.setPassengerList(
+        OrderMessage.transferPassenger(ticketOrderEntity.getPassengers()));
+    orderMessage.setStatus(status);
+    amqpMessageServiceImpl.sendMsg(JSON.toJSONString(orderMessage));
+  }
+
+  @Override
+  public List<Order> getOrders(Long userId) {
+    List<TicketOrderEntity> ticketOrderEntityList = orderRepository.findByUserId(userId);
+    return ticketOrderEntityList.stream().map(this::transferOrder).collect(Collectors.toList());
+  }
+
+  @Override
+  public FlightDetail getFlightDetail(String flight) {
+    return priceSeatManagerClient.getFlightDetail(flight);
+  }
+
+  private Order transferOrder(TicketOrderEntity ticketOrderEntity) {
+    Order order = Order.from(ticketOrderEntity);
+    order.setPassengerList(Order.transferPassenger(ticketOrderEntity.getPassengers()));
+    order.setOrderEventList(Order.transferOrderEvent(ticketOrderEntity.getEvents()));
+    return order;
+  }
+
+  public Order cancelOrder(Long orderId) {
+    // implement the logic of cancel order, make all tests in OrderServiceImplTest pass.
+    Optional<TicketOrderEntity> ticketOrderEntityOptional = orderRepository.findById(orderId);
+    if (!ticketOrderEntityOptional.isPresent()) {
+      throw new OrderNotFoundException("订单不存在");
     }
-
-    private TicketOrderEntity transferToOrderEntity(CreateOrderDto createOrderDto) {
-        return TicketOrderEntity.builder()
-                                .userId(createOrderDto.getUserId())
-                                .flight(createOrderDto.getFlight())
-                                .classType(createOrderDto.getClassType())
-                                .contactName(createOrderDto.getContactName())
-                                .contactMobile(createOrderDto.getContactMobile())
-                                .status(OrderStatusEnum.CREATED.name())
-                                .build();
+    TicketOrderEntity ticketOrderEntity = ticketOrderEntityOptional.get();
+    if (OrderStatusEnum.CANCELED.name().equals(ticketOrderEntity.getStatus())) {
+      throw new BusinessException(ErrorCode.ORDER_ALREADY_CANCELLED, "订单已取消");
     }
-
-    private void publish(TicketOrderEntity ticketOrderEntity, String status) {
-        OrderMessage orderMessage = OrderMessage.from(ticketOrderEntity);
-        orderMessage.setPassengerList(OrderMessage.transferPassenger(ticketOrderEntity.getPassengers()));
-        orderMessage.setStatus(status);
-        amqpMessageServiceImpl.sendMsg(JSON.toJSONString(orderMessage));
-    }
-
-    @Override
-    public List<Order> getOrders(Long userId) {
-        List<TicketOrderEntity> ticketOrderEntityList = orderRepository.findByUserId(userId);
-        return ticketOrderEntityList.stream().map(this::transferOrder).collect(Collectors.toList());
-    }
-
-    @Override
-    public FlightDetail getFlightDetail(String flight) {
-        return priceSeatManagerClient.getFlightDetail(flight);
-    }
-
-    private Order transferOrder(TicketOrderEntity ticketOrderEntity) {
-        Order order = Order.from(ticketOrderEntity);
-        order.setPassengerList(Order.transferPassenger(ticketOrderEntity.getPassengers()));
-        order.setOrderEventList(Order.transferOrderEvent(ticketOrderEntity.getEvents()));
-        return order;
-    }
-
-    public Order cancelOrder(Long orderId) {
-        return null;
-    }
+    ReleaseSeatRequest releaseSeatRequest =
+        new ReleaseSeatRequest(
+            ticketOrderEntity.getFlight(),
+            ticketOrderEntity.getClassType(),
+            ticketOrderEntity.getPassengers().size());
+    priceSeatManagerClient.releaseSeat(releaseSeatRequest);
+    ticketOrderEntity.setStatus(OrderStatusEnum.CANCELED.name());
+    orderRepository.save(ticketOrderEntity);
+    publish(ticketOrderEntity, OrderStatusEnum.CANCELED.name());
+    return transferOrder(ticketOrderEntity);
+  }
 }
--- src/test/java/com/tw/flyhigh/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/tw/flyhigh/service/impl/OrderServiceImplTest.java
@@ -2,6 +2,7 @@ package com.tw.flyhigh.service.impl;
 
 import com.tw.flyhigh.common.exception.BusinessException;
 import com.tw.flyhigh.common.exception.NoMoreSeatException;
+import com.tw.flyhigh.common.exception.OrderNotFoundException;
 import com.tw.flyhigh.dto.Order;
 import com.tw.flyhigh.dto.ReleaseSeatRequest;
 import com.tw.flyhigh.dto.ReserveSeatRequest;
@@ -85,5 +86,62 @@ class OrderServiceImplTest {
     void should_throw_exception_when_call_seat_manager_server_to_reserve_seat_failed() {
         when(priceSeatManagerClient.reserveSeat(any())).thenThrow(FeignException.FeignServerException.class);
         Assertions.assertThrows(FeignException.FeignServerException.class, () -> orderServiceImpl.createOrder(getCreateOrderDto()));
+    }
+
+    // add unit tests for cancel order
+    // AC1: After the user submits a request to cancel the order, the order is successfully cancelled.
+    @Test
+    void should_cancel_order_successful() {
+        // given
+        Long orderId = 1L;
+        TicketOrderEntity ticketOrderEntity = getOrderEntity();
+        when(orderRepositorySpy.findById(orderId)).thenReturn(Optional.of(ticketOrderEntity));
+        when(priceSeatManagerClient.reserveSeat(any())).thenReturn(true);
+        when(orderRepositorySpy.save(any())).thenReturn(any());
+
+        // when
+        orderServiceImpl.cancelOrder(orderId);
+
+        // then
+        verify(orderRepositorySpy, times(1)).findById(orderId);
+        verify(priceSeatManagerClient, times(1)).releaseSeat(releaseRequestCaptor.capture());
+        Assertions.assertEquals("MU2151", releaseRequestCaptor.getValue().getFlight());
+        Assertions.assertEquals(1, releaseRequestCaptor.getValue().getNumber());
+        verify(orderRepositorySpy, times(1)).save(order.capture());
+        Assertions.assertEquals("张三", order.getValue().getContactName());
+    }
+
+    // AC2: If the order does not exist, the system will return an error message.
+    @Test
+    void should_throw_exception_when_order_not_found() {
+        // given
+        Long orderId = 1L;
+        when(orderRepositorySpy.findById(orderId)).thenReturn(Optional.empty());
+
+        // when
+        Assertions.assertThrows(OrderNotFoundException.class, () -> orderServiceImpl.cancelOrder(orderId));
+
+        // then
+        verify(orderRepositorySpy, times(1)).findById(orderId);
+        verify(priceSeatManagerClient, never()).releaseSeat(any());
+        verify(orderRepositorySpy, never()).save(any());
+    }
+
+    // AC3: If the order has been cancelled, the system will return an error message.
+    @Test
+    void should_throw_exception_when_order_already_cancelled() {
+        // given
+        Long orderId = 1L;
+        TicketOrderEntity ticketOrderEntity = getOrderEntity();
+        ticketOrderEntity.setStatus("CANCELED");
+        when(orderRepositorySpy.findById(orderId)).thenReturn(Optional.of(ticketOrderEntity));
+
+        // when
+        Assertions.assertThrows(BusinessException.class, () -> orderServiceImpl.cancelOrder(orderId));
+
+        // then
+        verify(orderRepositorySpy, times(1)).findById(orderId);
+        verify(priceSeatManagerClient, never()).releaseSeat(any());
+        verify(orderRepositorySpy, never()).save(any());
     }
 }
